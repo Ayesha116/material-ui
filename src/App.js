177ceab41cb75b8d6f81a41da0e93fe94a253499
Ayesha116 <ayeshajawed1168@gmail.com>
2020-07-02 21:21:48 +0500

    practice material ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import React from "react";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  girdpart: {
+    backgroundColor : "purple",
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    // color: theme.palette.text.secondary,
+    color: "orange",
+    backgroundColor: "brown",
+  },
+}));
 
 function App() {
+
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root} >
+      <Grid container spacing={3}>
+        <Grid item xs={7}>
+          <Paper className={classes.paper}>ayesha</Paper>
+        </Grid>
+        <Grid item xs={5}>
+          <Paper className={classes.paper}>ariba</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>alishba</Paper>
+        </Grid>
+        
+      </Grid>
     </div>
   );
-}
+}      
+      
 
 export default App;
